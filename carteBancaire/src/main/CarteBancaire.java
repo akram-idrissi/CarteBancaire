3f7200bc15b5102452839dbfdc2e3b9ac456d6b6
Akram Idrissi <akram.mo.gb@gmail.com>
2023-12-12 21:52:13 +0100

    Ajout des attributs et accesseurs

--- carteBancaire/src/main/CarteBancaire.java
+++ carteBancaire/src/main/CarteBancaire.java
@@ -1,5 +1,61 @@
 package main;
 
+import java.time.LocalDate;
+
 public class CarteBancaire {
+	
+	public final String numeroCarte = "";
+    public final String titulaire = "";
+    public final LocalDate dateExpiration = null;
+    private int codeSecurite;
+    private boolean active;
+    private double plafond;
+    private boolean sansContact;
+    private double soldeDisponible;
+	public int getCodeSecurite() {
+		return codeSecurite;
+	}
+	public void setCodeSecurite(int codeSecurite) {
+		this.codeSecurite = codeSecurite;
+	}
+	public boolean isActive() {
+		return active;
+	}
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	public double getPlafond() {
+		return plafond;
+	}
+	public void setPlafond(double plafond) {
+		this.plafond = plafond;
+	}
+	public boolean isSansContact() {
+		return sansContact;
+	}
+	public void setSansContact(boolean sansContact) {
+		this.sansContact = sansContact;
+	}
+	public double getSoldeDisponible() {
+		return soldeDisponible;
+	}
+	public void setSoldeDisponible(double soldeDisponible) {
+		this.soldeDisponible = soldeDisponible;
+	}
+	public String getNumeroCarte() {
+		return numeroCarte;
+	}
+	public String getTitulaire() {
+		return titulaire;
+	}
+	public LocalDate getDateExpiration() {
+		return dateExpiration;
+	}
+    
+    
+	
+	
+	
+	
 
 }
